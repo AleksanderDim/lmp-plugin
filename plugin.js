955de187569c76eeffc68c44901700f117aeb156
AllDi <119818546+AleksanderDim@users.noreply.github.com>
2026-04-25 02:51:43 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -2,59 +2,71 @@
     'use strict';
 
     function startPlugin() {
-        Lampa.Noty.show('UA-Plugin: Спроба інтеграції в розділ Детально');
-
-        // Створюємо компонент, який відкриється при натисканні
+        // Реєструємо компонент, який буде показувати список озвучок
         Lampa.Component.add('ua_online_mod', function (object) {
             var scroll = new Lampa.Scroll({ mask: true, over: true });
+            
             this.create = function () {
                 var _this = this;
-                this.activity.loader(false);
-                scroll.clear();
-                var card = Lampa.Template.get('button', { 
-                    title: 'Пошук джерел для: ' + (object.movie.title || object.movie.name) 
-                });
-                scroll.append(card);
+                this.activity.loader(true);
+                
+                // Тестовий список джерел
+                setTimeout(function(){
+                    _this.activity.loader(false);
+                    var results = [
+                        {title: 'UA: Anitube', url: 'https://anitube.in.ua/'},
+                        {title: 'UA: UAKino', url: 'https://uakino.cx/'},
+                        {title: 'UA: Ashdi', url: 'https://ashdi.vip/'}
+                    ];
+                    _this.draw(results);
+                }, 800);
+
                 return scroll.render();
+            };
+
+            this.draw = function(data) {
+                scroll.clear();
+                var _this = this;
+                data.forEach(function(item) {
+                    var card = Lampa.Template.get('button', { title: item.title });
+                    card.on('hover:enter', function() {
+                        Lampa.Noty.show('Запуск пошуку на ' + item.title);
+                    });
+                    scroll.append(card);
+                });
             };
         });
 
-        // Слухаємо відкриття картки
+        // ПРАВИЛЬНИЙ МЕТОД ДЛЯ ОРИГІНАЛЬНОЇ LAMPA:
+        // Слухаємо подію відкриття картки і додаємо кнопку через вбудований метод
         Lampa.Listener.follow('full', function (e) {
             if (e.type == 'complite') {
-                // Шукаємо за текстом "Детально", оскільки класи можуть бути іншими
-                setTimeout(function() {
-                    // 1. Шукаємо блок, де написано "Детально"
-                    var detailSection = $("div:contains('Детально'), .full-info__title:contains('Детально')").parent();
-                    
-                    if (detailSection.length && !$('.view--ua-online').length) {
-                        // Створюємо кнопку, схожу на вашу "Дивитись"
-                        var btn = $('<div class="full-start__button selector view--ua-online" style="margin-top: 10px; background: #24b47e !important;"><span>UA Онлайн</span></div>');
-                        
-                        btn.on('hover:enter click', function () {
-                            Lampa.Activity.push({
-                                title: 'UA Онлайн',
-                                component: 'ua_online_mod',
-                                movie: e.data.movie
-                            });
-                        });
+                var btn = $('<div class="full-start__button selector view--ua-online"><span>UA Онлайн</span></div>');
+                
+                btn.on('hover:enter', function () {
+                    Lampa.Activity.push({
+                        url: '',
+                        title: 'UA Онлайн',
+                        component: 'ua_online_mod',
+                        movie: e.data.movie,
+                        page: 1
+                    });
+                });
 
-                        // Вставляємо кнопку прямо ПІД розділ "Детально" або поруч із "Дивитись"
-                        var mainButtons = $('.full-start__buttons, .movie-full__buttons');
-                        if (mainButtons.length) {
-                            mainButtons.append(btn);
-                        } else {
-                            detailSection.after(btn);
-                        }
-                        
-                        // Оновлюємо навігацію пульта
-                        Lampa.Controller.enable('full');
-                    }
-                }, 1000);
+                // В оригінальній Лампі ми шукаємо саме цей блок
+                var container = e.object.render().find('.full-start__buttons');
+                if (container.length && !container.find('.view--ua-online').length) {
+                    container.append(btn);
+                    // Важливо для ядра: оновлюємо навігацію конкретного об'єкта
+                    e.object.navigation(); 
+                }
             }
         });
+
+        Lampa.Noty.show('UA-Plugin: Інтегровано в ядро!');
     }
 
-    if (window.Lampa) startPlugin();
-    else document.addEventListener('appready', startPlugin);
+    // Офіційний запуск
+    if (window.appready) startPlugin();
+    else Lampa.Listener.follow('app', function (e) { if (e.type == 'ready') startPlugin(); });
 })();
