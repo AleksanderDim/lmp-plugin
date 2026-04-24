032398fe52b6a3fa34d5f95b16e7aa565dbbf7b8
AllDi <119818546+AleksanderDim@users.noreply.github.com>
2026-04-25 01:28:36 +0300

    Refactor plugin initialization and button creation

--- plugin.js
+++ plugin.js
@@ -1,35 +1,46 @@
 (function () {
-    'use strict';
+    var init = function () {
+        // Спроба вивести сповіщення через стандартний alert, якщо Noty не працює
+        try {
+            Lampa.Noty.show('UA-Plugin: Спроба запуску');
+        } catch(e) {
+            console.log('Lampa Noty не підтримується');
+        }
 
-    function init() {
-        // Повідомлення, яке має з'явитися вгорі екрана ПРИ ЗАПУСКУ
-        setTimeout(function() {
-            if (window.Lampa) {
-                Lampa.Noty.show('UA-Plugin: ЗВ’ЯЗОК ВСТАНОВЛЕНО');
-            }
-        }, 2000);
-
-        // Додаємо кнопку
-        Lampa.Listener.follow('full', function (e) {
-            if (e.type == 'complite') {
+        // Універсальний пошук контейнера для кнопок
+        var timer = setInterval(function () {
+            var container = $('.full-start__buttons, .movie-full__buttons, .buttons--full');
+            
+            if (container.length && !$('.view--ua-online').length) {
                 var btn = $('<div class="full-start__button selector view--ua-online"><span>UA Онлайн</span></div>');
                 
-                btn.on('hover:enter', function () {
-                    Lampa.Noty.show('Пошук запущено...');
+                // Стилізація для старих версій
+                btn.css({
+                    'background': 'rgba(255,255,255,0.1)',
+                    'padding': '10px 20px',
+                    'margin': '5px',
+                    'border-radius': '5px',
+                    'display': 'inline-block',
+                    'cursor': 'pointer'
                 });
-                
-                if (!$('.view--ua-online').length) {
-                    $('.full-start__buttons').append(btn);
-                }
-            }
-        });
-    }
 
-    // Запуск
-    if (window.appready) init();
+                btn.on('click hover:enter', function () {
+                    alert('Працює! Тепер ми знаємо, що код виконується.');
+                });
+
+                container.append(btn);
+            }
+        }, 1000); // Перевіряємо наявність кнопок кожну секунду
+    };
+
+    // Запуск без зайвих умов
+    if (window.Lampa) init();
     else {
-        document.addEventListener('appready', init);
-        // Резервний запуск через 3 секунди
-        setTimeout(init, 3000);
+        var waitLampa = setInterval(function() {
+            if (window.Lampa) {
+                clearInterval(waitLampa);
+                init();
+            }
+        }, 500);
     }
 })();
