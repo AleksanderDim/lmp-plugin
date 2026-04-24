d35ccf6c67e96b423a2ee48fa1125276dabf8b21
AllDi <119818546+AleksanderDim@users.noreply.github.com>
2026-04-25 02:44:55 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -2,7 +2,9 @@
     'use strict';
 
     function startPlugin() {
-        // 1. Створюємо компонент пошуку
+        Lampa.Noty.show('UA-Plugin: Спроба інтеграції в розділ Детально');
+
+        // Створюємо компонент, який відкриється при натисканні
         Lampa.Component.add('ua_online_mod', function (object) {
             var scroll = new Lampa.Scroll({ mask: true, over: true });
             this.create = function () {
@@ -17,47 +19,40 @@
             };
         });
 
-        // 2. ДОДАЄМО В КОНТЕКСТНЕ МЕНЮ (кнопка "Меню" або довге натискання)
-        Lampa.Listener.follow('app', function (e) {
-            if (e.type == 'contextmenu') {
-                // Додаємо пункт у список, який випадає
-                e.variables.menu.push({
-                    title: 'UA Онлайн (Пошук)',
-                    subtitle: 'Шукати українською',
-                    icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" fill="white"/></svg>',
-                    action: 'ua_online_go'
-                });
-            }
-        });
-
-        // Слухаємо натискання на наш пункт у меню
-        Lampa.Listener.follow('menu', function (e) {
-            if (e.action == 'ua_online_go') {
-                Lampa.Activity.push({
-                    title: 'UA Онлайн',
-                    component: 'ua_online_mod',
-                    movie: Lampa.Activity.active().card || {}
-                });
-            }
-        });
-
-        // 3. ТАКОЖ ДОДАМО В ПЕРШИЙ РЯДОК НАЛАШТУВАНЬ (як запасний вихід)
-        Lampa.Listener.follow('settings', function (e) {
-            if (e.type == 'open' && e.name == 'main') {
-                setTimeout(function(){
-                    var container = $('.settings-main');
-                    if (container.length && !$('.view--ua-set').length) {
-                        var item = $('<div class="settings-param selector view--ua-set"><div class="settings-param__name">UA Онлайн</div><div class="settings-param__value">Перевірити роботу</div></div>');
-                        item.on('hover:enter click', function(){
-                            Lampa.Noty.show('Плагін активний!');
+        // Слухаємо відкриття картки
+        Lampa.Listener.follow('full', function (e) {
+            if (e.type == 'complite') {
+                // Шукаємо за текстом "Детально", оскільки класи можуть бути іншими
+                setTimeout(function() {
+                    // 1. Шукаємо блок, де написано "Детально"
+                    var detailSection = $("div:contains('Детально'), .full-info__title:contains('Детально')").parent();
+                    
+                    if (detailSection.length && !$('.view--ua-online').length) {
+                        // Створюємо кнопку, схожу на вашу "Дивитись"
+                        var btn = $('<div class="full-start__button selector view--ua-online" style="margin-top: 10px; background: #24b47e !important;"><span>UA Онлайн</span></div>');
+                        
+                        btn.on('hover:enter click', function () {
+                            Lampa.Activity.push({
+                                title: 'UA Онлайн',
+                                component: 'ua_online_mod',
+                                movie: e.data.movie
+                            });
                         });
-                        container.prepend(item);
+
+                        // Вставляємо кнопку прямо ПІД розділ "Детально" або поруч із "Дивитись"
+                        var mainButtons = $('.full-start__buttons, .movie-full__buttons');
+                        if (mainButtons.length) {
+                            mainButtons.append(btn);
+                        } else {
+                            detailSection.after(btn);
+                        }
+                        
+                        // Оновлюємо навігацію пульта
+                        Lampa.Controller.enable('full');
                     }
-                }, 100);
+                }, 1000);
             }
         });
-
-        Lampa.Noty.show('UA-Plugin: Спробуйте довге натискання на фільм');
     }
 
     if (window.Lampa) startPlugin();
