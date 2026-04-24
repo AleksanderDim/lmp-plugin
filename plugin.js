d49d7a7e4be69fb844373ecbcb3687dc9c73436a
AllDi <119818546+AleksanderDim@users.noreply.github.com>
2026-04-25 02:55:27 +0300

    Improve plugin structure and button handling
    Refactor plugin initialization and button creation logic.

--- plugin.js
+++ plugin.js
@@ -2,71 +2,60 @@
     'use strict';
 
     function startPlugin() {
-        // Реєструємо компонент, який буде показувати список озвучок
+        // 1. Реєструємо компонент (вікно з результатами)
         Lampa.Component.add('ua_online_mod', function (object) {
             var scroll = new Lampa.Scroll({ mask: true, over: true });
-            
             this.create = function () {
                 var _this = this;
-                this.activity.loader(true);
-                
-                // Тестовий список джерел
-                setTimeout(function(){
-                    _this.activity.loader(false);
-                    var results = [
-                        {title: 'UA: Anitube', url: 'https://anitube.in.ua/'},
-                        {title: 'UA: UAKino', url: 'https://uakino.cx/'},
-                        {title: 'UA: Ashdi', url: 'https://ashdi.vip/'}
-                    ];
-                    _this.draw(results);
-                }, 800);
-
-                return scroll.render();
-            };
-
-            this.draw = function(data) {
+                this.activity.loader(false);
                 scroll.clear();
-                var _this = this;
-                data.forEach(function(item) {
-                    var card = Lampa.Template.get('button', { title: item.title });
-                    card.on('hover:enter', function() {
-                        Lampa.Noty.show('Запуск пошуку на ' + item.title);
-                    });
-                    scroll.append(card);
+                var card = Lampa.Template.get('button', { 
+                    title: 'Пошук для: ' + (object.movie.title || object.movie.name) 
                 });
+                scroll.append(card);
+                return scroll.render();
             };
         });
 
-        // ПРАВИЛЬНИЙ МЕТОД ДЛЯ ОРИГІНАЛЬНОЇ LAMPA:
-        // Слухаємо подію відкриття картки і додаємо кнопку через вбудований метод
-        Lampa.Listener.follow('full', function (e) {
-            if (e.type == 'complite') {
+        // 2. Використовуємо системний таймер для "впорскування"
+        // Це спрацює навіть якщо ядро перемальовує інтерфейс
+        setInterval(function() {
+            if ($('.full-start__buttons').length && !$('.view--ua-online').length) {
                 var btn = $('<div class="full-start__button selector view--ua-online"><span>UA Онлайн</span></div>');
                 
-                btn.on('hover:enter', function () {
+                btn.on('hover:enter click', function () {
+                    // Отримуємо дані фільму безпосередньо з активної картки
+                    var active_card = Lampa.Activity.active().card;
                     Lampa.Activity.push({
                         url: '',
                         title: 'UA Онлайн',
                         component: 'ua_online_mod',
-                        movie: e.data.movie,
-                        page: 1
+                        movie: active_card
                     });
                 });
 
-                // В оригінальній Лампі ми шукаємо саме цей блок
-                var container = e.object.render().find('.full-start__buttons');
-                if (container.length && !container.find('.view--ua-online').length) {
-                    container.append(btn);
-                    // Важливо для ядра: оновлюємо навігацію конкретного об'єкта
-                    e.object.navigation(); 
+                // Додаємо кнопку
+                $('.full-start__buttons').append(btn);
+                
+                // Змушуємо Лампу перерахувати кнопки для пульта
+                if (Lampa.Controller.enabled().name == 'full') {
+                    Lampa.Controller.enable('full');
                 }
             }
-        });
+        }, 1000);
 
-        Lampa.Noty.show('UA-Plugin: Інтегровано в ядро!');
+        Lampa.Noty.show('UA-Plugin: Спроба через системний цикл');
     }
 
-    // Офіційний запуск
+    // Офіційний метод запуску для ядра
     if (window.appready) startPlugin();
     else Lampa.Listener.follow('app', function (e) { if (e.type == 'ready') startPlugin(); });
+    
+    // Резервний запуск
+    setTimeout(function() {
+        if (!window.UA_PLUGIN_INITED) {
+            startPlugin();
+            window.UA_PLUGIN_INITED = true;
+        }
+    }, 3000);
 })();
