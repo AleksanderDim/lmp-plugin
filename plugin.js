d7ebdde84029ff88795f9d16a52ea5ae14b3dd23
AllDi <119818546+AleksanderDim@users.noreply.github.com>
2026-04-25 02:01:19 +0300

    Refactor UA plugin initialization and button handling

--- plugin.js
+++ plugin.js
@@ -1,46 +1,60 @@
 (function () {
-    var init = function () {
-        // Спроба вивести сповіщення через стандартний alert, якщо Noty не працює
-        try {
-            Lampa.Noty.show('UA-Plugin: Спроба запуску');
-        } catch(e) {
-            console.log('Lampa Noty не підтримується');
+    'use strict';
+
+    // Офіційний метод реєстрації плагіна в Lampa
+    Lampa.Plugins.add('ua_online_plugin', function (api) {
+        
+        // Функція створення компонента пошуку
+        function startPlugin() {
+            Lampa.Component.add('ua_online_mod', function (object) {
+                var scroll = new Lampa.Scroll({ mask: true, over: true });
+                
+                this.create = function () {
+                    var _this = this;
+                    this.activity.loader(true);
+                    
+                    // Поки що тестовий список, щоб перевірити, чи відкривається вікно
+                    setTimeout(function(){
+                        _this.activity.loader(false);
+                        _this.draw([{title: 'Пошук налаштовується...'}]);
+                    }, 500);
+
+                    return scroll.render();
+                };
+
+                this.draw = function(data) {
+                    scroll.clear();
+                    data.forEach(function(item) {
+                        var card = Lampa.Template.get('button', { title: item.title });
+                        scroll.append(card);
+                    });
+                };
+            });
+
+            // Додаємо кнопку в картку фільму
+            Lampa.Listener.follow('full', function (e) {
+                if (e.type == 'complite') {
+                    var btn = $('<div class="full-start__button selector view--ua-online"><span>UA Онлайн</span></div>');
+                    
+                    btn.on('hover:enter', function () {
+                        Lampa.Activity.push({
+                            url: '',
+                            title: 'UA Онлайн',
+                            component: 'ua_online_mod',
+                            movie: Lampa.Activity.active().card
+                        });
+                    });
+                    
+                    if (!$('.view--ua-online').length) {
+                        $('.full-start__buttons').append(btn);
+                    }
+                }
+            });
+
+            Lampa.Noty.show('UA Онлайн: АКТИВОВАНО');
         }
 
-        // Універсальний пошук контейнера для кнопок
-        var timer = setInterval(function () {
-            var container = $('.full-start__buttons, .movie-full__buttons, .buttons--full');
-            
-            if (container.length && !$('.view--ua-online').length) {
-                var btn = $('<div class="full-start__button selector view--ua-online"><span>UA Онлайн</span></div>');
-                
-                // Стилізація для старих версій
-                btn.css({
-                    'background': 'rgba(255,255,255,0.1)',
-                    'padding': '10px 20px',
-                    'margin': '5px',
-                    'border-radius': '5px',
-                    'display': 'inline-block',
-                    'cursor': 'pointer'
-                });
-
-                btn.on('click hover:enter', function () {
-                    alert('Працює! Тепер ми знаємо, що код виконується.');
-                });
-
-                container.append(btn);
-            }
-        }, 1000); // Перевіряємо наявність кнопок кожну секунду
-    };
-
-    // Запуск без зайвих умов
-    if (window.Lampa) init();
-    else {
-        var waitLampa = setInterval(function() {
-            if (window.Lampa) {
-                clearInterval(waitLampa);
-                init();
-            }
-        }, 500);
-    }
+        // Запускаємо логіку
+        startPlugin();
+    });
 })();
