f9184d1db5d3ed684045ad981fbbf0918b2d5d87
AllDi <119818546+AleksanderDim@users.noreply.github.com>
2026-05-03 17:04:44 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,61 +1,65 @@
 (function () {
     'use strict';
 
-    function startPlugin() {
-        // 1. Реєструємо компонент
+    function initPlugin() {
+        // Реєструємо компонент для відображення списку або інформації
         Lampa.Component.add('ua_online_mod', function (object) {
             var scroll = new Lampa.Scroll({ mask: true, over: true });
+            
             this.create = function () {
                 var _this = this;
                 this.activity.loader(false);
                 scroll.clear();
 
-                var card = Lampa.Template.get('button', { title: 'UA Онлайн: Меню працює' });
+                // Отримуємо назву фільму
+                var movieTitle = object.movie ? (object.movie.title || object.movie.name) : 'фільм';
+
+                var card = Lampa.Template.get('button', { title: 'Знайдено джерела для: ' + movieTitle });
                 scroll.append(card);
+
+                var btnSrc = Lampa.Template.get('button', { title: 'UAKino' });
+                btnSrc.on('hover:enter click', function () {
+                    // Відкриваємо джерело
+                    window.open('https://uakino.club', '_blank');
+                });
+                scroll.append(btnSrc);
 
                 return scroll.render();
             };
         });
 
-        // 2. Розширена функція вставки кнопки
+        // Функція для пошуку контейнера кнопок та вставки нової
         function injectButton() {
-            // Перевіряємо одразу кілька можливих контейнерів для кнопок у різних версіях Lampa
-            var containers = [
-                $('.full-start__buttons'),
-                $('.full-start__actions'),
-                $('.view--item .info__actions')
-            ];
-
+            // Шукаємо блок з діями (кнопки "Дивитись", "Трейлер" тощо)
+            var $container = $('.full-start__buttons');
             var activeActivity = Lampa.Activity.active();
             var activeMovie = activeActivity ? activeActivity.card : null;
 
-            containers.forEach(function ($container) {
-                if ($container.length && !$container.find('.view--ua-online').length) {
-                    var btn = $('<div class="full-start__button selector view--ua-online"><span>UA Онлайн</span></div>');
+            if ($container.length && !$container.find('.view--ua-online').length) {
+                var btn = $('<div class="full-start__button selector view--ua-online"><span>UA Онлайн</span></div>');
 
-                    btn.on('hover:enter click', function () {
-                        Lampa.Activity.push({
-                            title: 'UA Онлайн',
-                            component: 'ua_online_mod',
-                            movie: activeMovie
-                        });
+                btn.on('hover:enter click', function () {
+                    Lampa.Activity.push({
+                        title: 'UA Онлайн',
+                        component: 'ua_online_mod',
+                        movie: activeMovie
                     });
+                });
 
-                    $container.prepend(btn);
+                $container.prepend(btn);
 
-                    if (window.Lampa && Lampa.Controller) {
-                        Lampa.Controller.enable('full');
-                    }
-                    
-                    Lampa.Noty.show('UA Онлайн: Кнопку додано!');
+                if (window.Lampa && Lampa.Controller) {
+                    Lampa.Controller.enable('full');
                 }
-            });
+                
+                Lampa.Noty.show('UA-Plugin: Кнопка активована');
+            }
         }
 
-        // Запускаємо з більшою затримкою, щоб сторінка встигла повністю промалюватися
+        // Запускаємо через 3.5 секунди після завантаження для стабільності
         setTimeout(injectButton, 3500);
 
-        // Відстежуємо зміни в інтерфейсі (якщо користувач перемикає фільми)
+        // Відстежуємо зміни сторінок
         var observer = new MutationObserver(function (mutations) {
             injectButton();
         });
@@ -64,14 +68,15 @@
             childList: true,
             subtree: true
         });
-
+        
         Lampa.Noty.show('UA-Plugin: Ініціалізовано');
     }
 
+    // Запуск плагіна з перевіркою стану Lampa
     if (window.appready) {
-        startPlugin();
+        initPlugin();
     } else {
-        document.addEventListener('appready', startPlugin);
-        setTimeout(startPlugin, 3500);
+        document.addEventListener('appready', initPlugin);
+        setTimeout(initPlugin, 3500);
     }
 })();
